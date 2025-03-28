43c9fb09b6476f6794a65b366be237a597577d89
YuvaneshKS <yuvaneshkarunakaran@gmail.com>
2025-03-28 21:47:25 +0530

    Update myapp.py

--- src/myapp.py
+++ src/myapp.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,5 +8,5 @@ def home():
     return "Hello, World! Your Flask App is Running 🚀"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
-
+    port = int(os.getenv("PORT", 5000))  # Get the PORT from Render
+    app.run(debug=True, host="0.0.0.0", port=port)
